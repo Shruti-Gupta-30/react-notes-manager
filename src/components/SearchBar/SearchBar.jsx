7cb742bdc267c9bd3591fc7dea30860f7874d125
Shruti Gupta <guptashruti.0310@gmail.com>
2023-09-23 03:57:52 +0530

    Created a  responsive signin page

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -5,12 +5,7 @@ export function SearchBar({ onTextChange, placeholder }) {
 	return (
 		<>
 			<SearchIcon size={25} className={s.icon} />
-			<input
-				type="text"
-				className={s.input}
-				onChange={(e) => onTextChange(e.target.value)}
-				placeholder={placeholder}
-			/>
+			<Input onTextChange={onTextChange} placeholder={placeholder} />
 		</>
 	);
 }
---
+++ src/pages/Signin/Signin.jsx
@@ -0,0 +1,39 @@
+import { ButtonPrimary } from "components/ButtonPrimary/ButtonPrimary";
+import { Link } from "react-router-dom";
+import s from "./style.module.css";
+import { Input } from "components/Input/Input";
+import { AuthLayout } from "Layouts/AuthLayout/AuthLayout";
+import { useState } from "react";
+
+export function Signin() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const submit = (e) => {
+		e.preventDefault();
+	};
+
+	const form = (
+		<div className={s.formContainer}>
+			<h2 className={s.title}>
+				Sign in <br />
+				to access your team notes
+			</h2>
+			<form onSubmit={submit} className={s.formGroup}>
+				<Input placeholder={"Email"} onTextChange={setEmail} />
+				<Input
+					placeholder={"Password"}
+					type="password"
+					onTextChange={setPassword}
+				/>
+				<ButtonPrimary type="submit" className={s.button}>
+					Sign in!
+				</ButtonPrimary>
+				<span>
+					Don't have an account yet? <Link to={"/signup"}>Signup</Link>
+				</span>
+			</form>
+		</div>
+	);
+	return <AuthLayout>{form}</AuthLayout>;
+}
