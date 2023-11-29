640678a279778197de2aa8f3e1e1e2e6168ea699
Shruti Gupta <guptashruti.0310@gmail.com>
2023-11-30 03:05:46 +0530

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -14,11 +14,12 @@ import {
 	REGISTER,
 } from "redux-persist";
 
+//1 Combine the reducers (slices content) into a single reducer
 const rootReducer = combineReducers({
 	notesSlice: notesReducer,
 	authSlice: authReducer,
 });
-
+//2 Create a basic configuration to tell redux to use the local storage
 const persistConfig = {
 	key: "root",
 	version: 1,
@@ -26,8 +27,10 @@ const persistConfig = {
 	whitelist: ["authSlice"],
 };
 
+//3 Persist the reducers
 const persistedReducers = persistReducer(persistConfig, rootReducer);
 
+//4 Send the persisted reducers to the store and 8 Tell Redux to ignore all the actions sent by redux-persist
 const store = configureStore({
 	reducer: persistedReducers,
 	middleware: (getDefaultMiddleware) =>
@@ -38,6 +41,10 @@ const store = configureStore({
 		}),
 });
 
+//5 Create a persisted version of the store
 const persistor = persistStore(store);
 
+//6 Export the persisted version of the store
 export { store, persistor };
+
+//7 Use the redusx Persist Gate component to give your app access to persisted store (in index.js root file)
